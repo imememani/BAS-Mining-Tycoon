b510409ca0fb8924245708230d267a9c7c890eb0
MemeMan <52583316+imememani@users.noreply.github.com>
2023-01-25 17:02:51 +0000

    Minor Tweak

--- BAS-Mining-Tycoon-Unity/Assets/Mining Tycoon/Scripts/Globals.cs
+++ BAS-Mining-Tycoon-Unity/Assets/Mining Tycoon/Scripts/Globals.cs
@@ -1,11 +1,11 @@
 public enum Tier
 {
+    Stone = 4,
     Copper = 0,
+    Tin = 5,
     Iron = 1,
+    Silver = 6,
     Ruby = 2,
     Gold = 3,
-    Stone = 4,
-    Tin = 5,
-    Silver = 6,
     Uranium = 7
 }
